33197324f52cd6af76597b2640d1488458ae1a32
valluruvedanath <valluruvedanath@gmail.com>
2018-02-18 14:25:50 +0530

    code format.

--- app/controllers/api/admin/events_controller.rb
+++ app/controllers/api/admin/events_controller.rb
@@ -3,23 +3,14 @@ module Api
         class EventsController < ApplicationController
           skip_before_action :verify_authenticity_token
 
-          #respond_to :json
-
           def index
-            #if current_user.present? && current_user.user_type.eql?("ADMIN")
               @events = Event.all
-           # else
-            #  @events = "current user not an admin"
-           # end
             respond_to do |format|
-
-              format.json { render json: @events }
+            format.json { render json: @events }
             end
           end
           def create_event_by_admin
             event = Event.new
-            puts "^^^^^^^^^^^^^^^^^^^"
-            puts "params[:description]"
             event.description = params[:description]
             event.event_type = params[:event_type]
             event.event_date = params[:event_date]
@@ -52,6 +43,7 @@ module Api
               event_subscription.is_removed_by_admin = true
               event_subscription.save
             end
+            render :json =>{ :status => "event removed by admin"}
           end
           def confirmed_list
             event_subscription = EventSubscription.where("is_confirmed_by_admin=? and is_removed_by_admin=?", true, false)
@@ -65,38 +57,18 @@ module Api
               format.json { render :json => {:removed_list => event_subscription.present? ? event_subscription : "there is no removed list. " }}
             end
           end
-         def attendees_willing_count
-           event_subscription = EventSubscription.where("is_intrested=?", true)
-           respond_to do |format|
-             format.json { render :json => {:willing_count => event_subscription.present? ? event_subscription.size() : 0  }}
-           end
-         end
+          def attendees_willing_count
+            event_subscription = EventSubscription.where("is_intrested=?", true)
+            respond_to do |format|
+              format.json { render :json => {:willing_count => event_subscription.present? ? event_subscription.size() : 0  }}
+            end
+          end
           def attendees_not_willing_count
             event_subscription = EventSubscription.where("is_intrested=?", false)
             respond_to do |format|
               format.json { render :json => {:not_willing_count => event_subscription.present? ? event_subscription.size() : 0  }}
             end
           end
-          def new
-            @event = Event.new
-          end
-
-          def show
-            respond_with Event.find(params[:id])
-          end
-
-          def create
-              respond_with Event.create(params[:event])
-          end
-
-          def update
-            respond_with Event.update(params[:id], params[:event])
-          end
-
-          def destroy
-            respond_with Event.destroy(params[:id])
-          end
-
         end
   end
 end
--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -6,7 +6,6 @@ module Api
       #respond_to :json
 
       def index
-       # if current_user.present? && current_user.user_type.eql?("USER")
         @events = []
         event_subscriptions = EventSubscription.where('user_id=? and is_intrested=?', current_user.id, true) if current_user.present?
         if event_subscriptions.present?
@@ -14,32 +13,27 @@ module Api
             @events << event_subscription.event
           end
         end
-       # else
-       #   @events = "current user not an admin"
-       # end
         respond_to do |format|
-
           format.json { render json: @events.present? ? @events : "There is no events for you. " }
         end
       end
-     def user_attend_to_event
-       status = ""
-       #user = User.find_by_id(params[:user_id]) if params[:user_id].present?
-       event = Event.find_by_id(params[:event_id])if params[:event_id].present?
 
-       if current_user.present? && event.present?
-       event_subscription = EventSubscription.find_by_event_id_and_user_id(event.id, current_user.id)
-       event_subscription = EventSubscription.new if !event_subscription.present?
-       event_subscription.event_id = event.id
-       event_subscription.user_id = current_user.id
-       event_subscription.is_intrested = true
-       event_subscription.save
-         status = "User was successfully added to event. "
-       else
-         status = "User was not added to event"
-       end
-       render json: status
-     end
+      def user_attend_to_event
+        status = ""
+        event = Event.find_by_id(params[:event_id])if params[:event_id].present?
+        if current_user.present? && event.present?
+          event_subscription = EventSubscription.find_by_event_id_and_user_id(event.id, current_user.id)
+          event_subscription = EventSubscription.new if !event_subscription.present?
+          event_subscription.event_id = event.id
+          event_subscription.user_id = current_user.id
+          event_subscription.is_intrested = true
+          event_subscription.save
+          status = "User was successfully added to event. "
+        else
+          status = "User was not added to event"
+        end
+        render json: status
+      end
       def user_canceled_event
         status = ""
         event_subscription = EventSubscription.find_by_event_id_and_user_id_and_is_intrested(params[:event_id], current_user.id, true) if current_user.present?
@@ -51,21 +45,6 @@ module Api
           status = "user not found. "
         end
         render json: status
-      end
-      def show
-        respond_with Event.find(params[:id])
-      end
-
-      def create
-        respond_with Event.create(params[:event])
-      end
-
-      def update
-        respond_with Event.update(params[:id], params[:event])
-      end
-
-      def destroy
-        respond_with Event.destroy(params[:id])
       end
     end
   end
